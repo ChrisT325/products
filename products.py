bef70d78a4bd9c77f273a2e0f38465ff5b5709ca
Chris Tsai <g147573@gmail.com>
2021-04-21 20:49:49 +0800

    Modified Ver.

--- products.py
+++ products.py
@@ -1,9 +1,20 @@
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price= line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+
+products = []
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	p = []
 	p.append(name) # p = [name, price]
 	p.append(price)
@@ -13,8 +24,9 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
 
 
